b533c9dcba4178f5f190c7e8f1fb945c876a6b54
“marijoroldan” <marijoroldan42@gmail.com>
2026-06-19 17:55:14 -0300

    comienzo sistema

---
+++ SistPersonal/Program.cs
@@ -0,0 +1,61 @@
+﻿using DatosEmpleado;
+
+DateTime fechaNac;
+string fechaProvisoria;
+bool fechaValida;
+Empleado[] arrEmp = new Empleado[3];
+
+//==================================================================================
+//                        ACA INGRESO LOS DATOS MANUALMENTE
+//==================================================================================
+
+// for (int i = 0; i < 3; i++)
+// {
+//     Empleado miEmpleado = new Empleado();
+//     miEmpleado.Nombre = "Maria";
+//     miEmpleado.Apellido = "Roldán";
+//     arrEmp[i]=miEmpleado;
+// }
+// =================================================================================
+//                   ESTO HAGO SI QUIERO QUE EL USUARIO INGRESE DATOS
+// =================================================================================
+for (int i = 0; i < 3; i++)
+{
+    arrEmp[i] = new Empleado();
+    Console.WriteLine("INGRESO DE DATOS DEL EMPLEADO");
+    Console.WriteLine("Nombre: ");
+    arrEmp[i].Nombre = Console.ReadLine();
+    Console.WriteLine("Apellido: ");
+    arrEmp[i].Apellido = Console.ReadLine();
+    Console.WriteLine("Fecha de Nacimiento: ");
+
+    do
+    {
+        Console.Write("Ingrese fecha de nacimiento (DD/MM/AAAA): ");
+        fechaProvisoria = Console.ReadLine();
+
+        // Intenta traducir el texto 'entrada' y si puede, guarda el objeto en 'fechaNac'
+        fechaValida = DateTime.TryParse(fechaProvisoria, out fechaNac);
+
+        if (!fechaValida)
+        {
+            Console.WriteLine("¡Formato incorrecto! Use números separados por barras (Ej: 15/07/2002).");
+        }
+    } while (!fechaValida);
+
+    // Una vez que salió del do-while, guardás esa fecha limpia en la propiedad del empleado
+    arrEmp[i].FechaNacimiento = fechaNac;
+
+    Console.WriteLine("Estado Civil: ");
+    arrEmp[i].EstadoCivil=Console.ReadLine();
+    
+}
+
+foreach (Empleado emp in arrEmp)
+{
+    Console.WriteLine("===========================================");
+    Console.WriteLine("                  DATOS DEL EMPLEADO ");
+    Console.WriteLine("===========================================");
+    Console.WriteLine($"Nombre: " + emp.Nombre);
+    Console.WriteLine("Apellido: " + emp.Apellido);
+}
---
+++ SistPersonal/sistema.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace DatosEmpleado;
+class Empleado
+{
+    string nombre;
+    string apellido;
+    DateTime fechaNacimiento;
+    char estadoCivil;
+    DateTime fechaDeIngreso;
+    double sueldoBasico;
+    Cargos cargo;
+
+    
+    // Propiedades públicas para poder asignar y leer los datos desde Program.cs (Encapsulamiento)
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
+        public DateTime FechaDeIngreso { get => fechaDeIngreso; set => fechaDeIngreso = value; }
+        public double SueldoBasico { get => sueldoBasico; set => sueldoBasico = value; }
+        public Cargos Cargo { get => cargo; set => cargo = value; }
+    public void Antiguedad(DateTime fechaDeIngreso)
+    {
+        double Antiguedad = DateTime.Today.Year - fechaDeIngreso.Year;
+    }
+    public void edad(DateTime fechaNacimiento)
+    {
+        int edad = DateTime.Today.Year - fechaNacimiento.Year;
+    }
+    public void jubilacion(int edad)
+    {
+        int jubilacion= 65-edad;
+    }
+}
+
+public enum Cargos
+{
+    Auxiliar,
+    Administrativo,
+    Ingeniero,
+    Especialista,
+    Investigador
+}
+    
+
